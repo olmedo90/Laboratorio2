43f2cc5f833c6584937a594c4643a861b12b6e4d
Primo5 <primocincko@gmail.com>
2022-05-11 19:59:32 -0400

    Laboratorio2_PrimoCincko

---
+++ src/App.ts
@@ -0,0 +1,31 @@
+import dotevn from "dotenv";
+import express, {Express} from "express";
+dotevn.config();
+class App {
+    private app: Express;
+    private port: number;
+    constructor() {
+        this.app = express();
+        this.port = Number(process.env.PORT) || 8000;
+        this.configure();
+        this.startModules();
+    }
+    private configure() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded());
+    }
+    private startModules() {
+    console.log("Load Modules!");
+    //codigo de los diferentes modulos de proyecto
+    this.app.get("/", (request, response) => {
+    response.send("Lo lograste primo lo lograste");
+    })
+}
+public getApp() {
+return this.app;
+}
+public getPort() {
+return this.port;
+}
+}
+export default App;
